3880e595931a6b5bb898e524bd59540cd9659267
SirMathhman <minecraftmathhman@gmail.com>
2019-05-12 17:20:18 -0400

    Finish

--- src/test/java/com/meti/color/BlackWindowTest.java
+++ src/test/java/com/meti/color/BlackWindowTest.java
@@ -8,7 +8,10 @@ import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBuffer;
 import java.awt.image.DataBufferInt;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.function.Consumer;
 
 /**
@@ -29,29 +32,23 @@ public class BlackWindowTest {
         SwingWindow window = new SwingWindow(WIDTH, HEIGHT);
         window.show();
 
-        Context context = window.getContext();
-
-
-        Canvas canvas = new Canvas();
-        frame.add(canvas);
-        frame.setVisible(true);
-
-        canvas.createBufferStrategy(3);
-        BufferStrategy strategy = canvas.getBufferStrategy();
-        Renderer renderer = new Renderer(frame, strategy);
-
-        ScheduledExecutorService service = Executors.newScheduledThreadPool(2);
-        Future<Renderer> rendererFuture = service.submit(renderer);
-
-        FramesPerSecondWriter writer = new FramesPerSecondWriter(renderer);
-        Future<?> writerFuture = service.submit(writer);
-        Consumer<WindowEvent> onWindowClosing = new BlackWindowClosingConsumer(writer, renderer, writerFuture, rendererFuture, service);
-        frame.addWindowListener(new FunctionalWindowAdapter().setOnWindowClosing(onWindowClosing));
+        Renderer<SwingContext> renderer = window.getRenderer();
     }
 
 
+    public interface Context {
+        void render();
 
-    public abstract class Renderer<C extends Context> implements Callable<Renderer<C>> {
+        int getHeight();
+
+        int getRGB(int x, int y);
+
+        int getWidth();
+
+        Context setRGB(int x, int y, int rgb);
+    }
+
+    public class Renderer<C extends Context> implements Callable<Renderer<C>> {
         private boolean running = true;
         private final C context;
 
@@ -62,113 +59,11 @@ public class BlackWindowTest {
         @Override
         public Renderer<C> call() {
             while(running){
-                render(context);
+                context.render();
             }
             return this;
         }
-
-        public abstract void render(C context);
     }
-
-    public interface Context {
-        int getHeight();
-
-        int getRGB(int x, int y);
-
-        int getWidth();
-
-        Context setRGB(int x, int y, int rgb);
-    }
-
-/*
-    private static class FramesPerSecondWriter implements Runnable {
-        private final Renderer renderer;
-        private boolean running;
-
-        private FramesPerSecondWriter(Renderer renderer) {
-            this.renderer = renderer;
-            this.running = true;
-        }
-
-        @Override
-        public void run() {
-            long before = System.nanoTime();
-            while (running) {
-                long now = System.nanoTime();
-                if (now - before > Math.pow(10, 9)) {
-                    long framesElapsed = renderer.readFramesElapsed();
-                    System.out.println(framesElapsed);
-
-                    before = now;
-                }
-            }
-        }
-
-        void stop() {
-            this.running = false;
-        }
-    }
-*/
-
-  /*  private static class Renderer implements Callable<Renderer> {
-        private final JFrame frame;
-        private final BufferStrategy strategy;
-        private long framesElapsed;
-
-        private BufferedImage image;
-        private boolean running;
-
-        private Renderer(JFrame frame, BufferStrategy strategy) {
-            this.frame = frame;
-            this.strategy = strategy;
-            this.framesElapsed = 0;
-
-            this.image = scaleImage(frame.getWidth(), frame.getHeight());
-            this.running = true;
-        }
-
-        private BufferedImage scaleImage(int width, int height) {
-            return new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        }
-
-        @Override
-        public Renderer call() {
-            while (running) {
-                do {
-                    do {
-                        renderFrame();
-                        framesElapsed++;
-                    } while (strategy.contentsRestored());
-                    strategy.show();
-                } while (strategy.contentsLost());
-            }
-            return this;
-        }
-
-        private void renderFrame() {
-            if (frame.getWidth() != image.getWidth()) {
-                this.image = scaleImage(frame.getWidth(), image.getHeight());
-            }
-
-            if (frame.getHeight() != image.getHeight()) {
-                this.image = scaleImage(image.getWidth(), frame.getHeight());
-            }
-
-            Graphics graphics = strategy.getDrawGraphics();
-            graphics.drawImage(image, 0, 0, null);
-            graphics.dispose();
-        }
-
-        long readFramesElapsed() {
-            long toReturn = this.framesElapsed;
-            this.framesElapsed = 0;
-            return toReturn;
-        }
-
-        void stop() {
-            this.running = false;
-        }
-    }*/
 
     private static class FunctionalWindowAdapter extends WindowAdapter {
         private Consumer<WindowEvent> onWindowClosing;
@@ -183,43 +78,6 @@ public class BlackWindowTest {
         public FunctionalWindowAdapter setOnWindowClosing(Consumer<WindowEvent> onWindowClosing) {
             this.onWindowClosing = onWindowClosing;
             return this;
-        }
-    }
-
-    private static class BlackWindowClosingConsumer implements Consumer<WindowEvent> {
-        private final FramesPerSecondWriter writer;
-        private final Renderer renderer;
-        private final Future<?> writerFuture;
-        private final Future<Renderer> rendererFuture;
-        private final ScheduledExecutorService service;
-
-        public BlackWindowClosingConsumer(FramesPerSecondWriter writer, Renderer renderer, Future<?> writerFuture, Future<Renderer> rendererFuture, ScheduledExecutorService service) {
-            this.writer = writer;
-            this.renderer = renderer;
-            this.writerFuture = writerFuture;
-            this.rendererFuture = rendererFuture;
-            this.service = service;
-        }
-
-        @Override
-        public void accept(WindowEvent e12) {
-            System.out.println(e12);
-            writer.stop();
-            renderer.stop();
-
-            writerFuture.cancel(true);
-            rendererFuture.cancel(true);
-
-            service.shutdownNow();
-            try {
-                boolean terminated = service.awaitTermination(1000, TimeUnit.MILLISECONDS);
-                if (!terminated) {
-                    throw new TimeoutException("Termination timeout timed-out.");
-                }
-            } catch (Exception e1) {
-                e1.printStackTrace();
-            }
-            System.exit(0);
         }
     }
 
@@ -239,18 +97,13 @@ public class BlackWindowTest {
 
         @Override
         public Renderer<SwingContext> getRenderer() {
-            return new Renderer<>(context) {
-                @Override
-                public void render(SwingContext context) {
-
-                }
-            };
+            return new Renderer<>(context);
         }
 
         @Override
-        public Context getContext() {
+        public SwingContext getContext() {
             if (context == null) {
-                context = new FrameSwingContext();
+                context = new SwingContext(frame.getBufferStrategy());
             }
             return context;
         }
@@ -259,24 +112,34 @@ public class BlackWindowTest {
         public void show() {
             frame.setVisible(true);
         }
-
-        private class FrameSwingContext extends SwingContext {
-            @Override
-            public void addCanvas(Canvas canvas) {
-                frame.add(canvas);
-            }
-        }
     }
 
-    private abstract class SwingContext implements Context {
+    private class SwingContext implements Context {
     private final BufferStrategy strategy;
         private BufferedImage image;
         private int width;
         private int height;
 
-        protected SwingContext() {
+        protected SwingContext(BufferStrategy strategy) {
+            this.strategy = strategy;
             this.width = 0;
             this.height = 0;
+        }
+
+        @Override
+        public void render() {
+            do {
+                do {
+                    renderFrame();
+                } while (strategy.contentsRestored());
+                strategy.show();
+            } while (strategy.contentsLost());
+        }
+
+        private void renderFrame() {
+            Graphics graphics = strategy.getDrawGraphics();
+            graphics.drawImage(image, 0, 0, null);
+            graphics.dispose();
         }
 
         @Override
